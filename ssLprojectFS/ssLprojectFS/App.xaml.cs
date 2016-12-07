6e0bd2146c6f12cb58e6d847ff6e95e564153e0b
Oleksandr Chaban <ochab@softserveinc.com>
2016-12-07 19:28:43 +0200

    Added settings and Unity(IoC-Container, DI, SL).

--- ssLprojectFS/ssLprojectFS/App.xaml.cs
+++ ssLprojectFS/ssLprojectFS/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Xamarin.Forms;
 using Microsoft.Practices.Unity;
+using Microsoft.Practices.ServiceLocation;
 
 namespace ssLprojectFS
 {
@@ -9,11 +10,21 @@ namespace ssLprojectFS
 		{
 			InitializeComponent();
 
+			var container = new UnityContainer();
+			this.RegisterTypes(container);
+			var locator = new UnityServiceLocator(container);
+			ServiceLocator.SetLocatorProvider(() => locator);
+
 			var navPage = new NavigationPage();
 			var welPage = new WelcomePage(navPage);
 			navPage.PushAsync(welPage);
 
 			MainPage = navPage;
+		}
+
+		private void RegisterTypes(IUnityContainer container)
+		{
+			container.RegisterType<IPersonFacade, PersonFacade>();
 		}
 
 		protected override void OnStart()
